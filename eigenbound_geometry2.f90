e40dfdefc4d36d50b570ca84d34bb14629e0df87
superaxelink <superaxelink@users.noreply.github.com>
2015-03-11 11:01:27 -0600

    Create eigenbound_geometry2.f90

---
+++ eigenbound_geometry2.f90
@@ -0,0 +1,15 @@
+
+
+  subroutine eigenbound_geometry2
+  real(8) dr_Aa, dr_K, dr_Phi, dr_B
+  real(8) Aa, B, r
+  
+
+  r=
+  Aa=
+  dr=
+  dr_K=0.
+  dr_Phi=
+  dr_b=
+  dr_Aa=2/3*dr_K+6*Aa*dr_Phi+2/3*Aa*(2*1/r+dr_b/b)
+  Aa=Aa+dr*dr_Aa
